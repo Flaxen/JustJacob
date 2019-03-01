668b300458533421a79af1c116363adce401decb
Flaxen <alexander.carlsson@live.se>
2019-03-01 17:51:46 +0100

    added main function currently running test programs for the sensor data interpreter

--- main.c
+++ main.c
@@ -4,42 +4,37 @@
 #include "displayFunctions.h"
 #include "i2cFunctions.h"
 #include "LSM6DS3addresses.h"
+#include "gameFunctions.h"
 
 int main(void) {
 
-		// cheaty things we "borrowed"
-		uint16_t temp;
-		char buf[32], *s, *t;
+	// cheaty things we "borrowed"
+	char buf[32], *s, *t;
 
 	spiInit();
 	i2cInit();
 
 	display_init();
 
-	// display_string(0, "Gronsaker");
-	// display_string(1, "");
-	// display_string(2, "");
-	// display_string(3, "");
-	// display_update();
-
 	while(1) {
+		updateTiltPos();
 
 		displayTest();
 		display_image(0, icon);
 
-		// temp = read(0x24);
-		//
-		// s = fixed_to_string(temp, buf);
-		//
+		if(xMoveDirection() > 0) {
+			TRISFCLR = 0x1; //lampor
+			PORTFSET = 0x1;
+		} else {
+			TRISFCLR = 0x1; //lampor
+			PORTFCLR = 0x1;
+		}
+
 		// display_string(1, s);
 		// display_update();
-		delay(1000000);
+		delay(10000);
 
-
-		delay(8000);
 	}
-
-
 
 	return 0;
 }
