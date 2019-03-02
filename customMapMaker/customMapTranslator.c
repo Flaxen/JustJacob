394654e4a980d4ae469e19279ffc5e5e611e6f19
Flaxen <alexander.carlsson@live.se>
2019-03-02 19:19:05 +0100

    added implementations for custom map importations

---
+++ customMapMaker/customMapTranslator.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define FILENAME "customMap.txt"
+
+int main(int argc, char *args[]) {
+
+  uint8_t tempArr[128*4];
+  int len = 128*8*4;
+  short *stringIn = malloc(len * sizeof(short));
+
+  char ch;
+  char file_name[] = FILENAME;
+  FILE *fp;
+
+  fp = fopen(file_name, "r");
+
+  for(int i = 0; i < len; i++) {
+    stringIn[i] = (fgetc(fp)-'0');
+  }
+  fclose(fp);
+
+  for (int i = 0; i < 128*4; i++) {
+    *(tempArr+i) = 0;
+  }
+
+  // makes string compact
+  for (int k = 0; k < 4; k++) {
+    for (int i = 0; i < 128; i++) {
+      for (int j = 0; j < 8; j++) {
+
+        tempArr[i+(k*128)] += ((stringIn[128*8*k+(i+128*j)])*(1<<j));
+
+      }
+    }
+  }
+
+
+  // prints compacted string as ready input array.
+  int printedValues = 0;
+  for (int i = 0; i < 128*4; i++) {
+    printf("%d, ", tempArr[i]);
+    printedValues++;
+  }
+  printf("\n\nPrinted values: %d\n", printedValues);
+
+  return 0;
+}
--- settings.h
+++ settings.h
@@ -1,8 +1,9 @@
-
+#include <stdint.h>
+#include "customMapMaker/customMaps.c"
 
 // Player Settings
-const int PLAYER_WIDTH = 3;
-const int PLAYER_HEIGHT = 3;
+const int PLAYER_WIDTH = 2;
+const int PLAYER_HEIGHT = 2;
 
 const int PLAYER_SPEED = 1;
 
@@ -10,5 +11,7 @@ const int PLAYER_SPEED = 1;
 const int SCREEN_WIDTH = 128;
 const int SCREEN_HEIGHT = 32;
 
-const int START_COORDINATE_X = 125;
+const int START_COORDINATE_X = 126;
 const int START_COORDINATE_Y = 0;
+
+// MAP CHOICE CAN BE FOUND IN displayFunctions
