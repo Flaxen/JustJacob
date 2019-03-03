72938099d0cd2c70e0c1c47c7593a6fbce043e99
Flaxen <alexander.carlsson@live.se>
2019-03-03 04:29:02 +0100

    updated settings

--- settings.h
+++ settings.h
@@ -14,4 +14,9 @@ const int SCREEN_HEIGHT = 32;
 const int START_COORDINATE_X = 126;
 const int START_COORDINATE_Y = 0;
 
+// Tileset Settings
+const int TILE_WIDTH = 23;
+const int TILE_HEIGHT = 21;
+
+
 // MAP CHOICE CAN BE FOUND IN displayFunctions
